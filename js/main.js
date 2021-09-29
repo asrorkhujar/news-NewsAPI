01fce032499c80208feb16590abf340a748cbf1b
coder-1990 <asrorakmalovich@gmail.com>
2021-09-30 01:39:03 +0500

    change styles

--- js/main.js
+++ js/main.js
@@ -1,37 +1,52 @@
-const elSearchForm = document.querySelector('.js-news-search-form');
-const elInput = document.querySelector('.search-input');
+const NEWS_API_KEY = 'bfc1f9b2dc384d1081e6f5a137b7288f';
+const TOP_HEADLINES_URL = `https://newsapi.org/v2/top-headlines?country=ru&apiKey=${NEWS_API_KEY}`;
+const NEWS_SEARCH_URL = `https://newsapi.org/v2/everything?apiKey=${NEWS_API_KEY}`;
+
+
+const elSearchForm = document.querySelector('.js-search-form');
+const elSearchInput = document.querySelector('.js-search-input');
+
 const elNewsList = document.querySelector('.news-list');
+const elNewsItemTemplate = document.querySelector('#news-item-template').content;
 
-elSearchForm.addEventListener('submit', onNewsSearchSubmit)
-
-function onNewsSearchSubmit(evt) {
-
-  if (elInput.value == '') {
-    alert('Biror narsani qidiruvga yozing!')
-    return
-  }
+function showNews (articles) {
   elNewsList.innerHTML = '';
+
+  const elNewsListFragment = document.createDocumentFragment();
+  articles.forEach(article => {
+    const elNewsItem = elNewsItemTemplate.cloneNode(true);
+    elNewsItem.querySelector('a').href = article.url;
+    elNewsItem.querySelector('.news-img').src = article.urlToImage;
+    elNewsItem.querySelector('h3').textContent = article.title;
+    elNewsItem.querySelector('p').textContent = article.source.name;
+    // elNewsItem.querySelector('.news-author').textContent = article.author;
+
+    elNewsListFragment.appendChild(elNewsItem);
+  });
+
+  elNewsList.appendChild(elNewsListFragment);
+}
+
+function getNewsJSON (url, callbackFn) {
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    if (data.status === 'ok') {
+      callbackFn(data.articles);
+    }
+  });
+}
+
+function onSearchFormSubmit (evt) {
   evt.preventDefault();
 
-  const apiKey = 'bfc1f9b2dc384d1081e6f5a137b7288f'
-  let topic = elInput.value;
+  const urlForSearch = `${NEWS_SEARCH_URL}&q=${elSearchInput.value.trim()}`;
 
-  let url = `https://newsapi.org/v2/everything?q=${topic}&from=2021-09-27&to=2021-09-27&sortBy=popularity&apiKey=${apiKey}&language=ru`;
-
-  fetch(url).then((res) => {
-    return res.json()
-  }).then((data) => {
-    data.articles.forEach(article => {
-      let li = document.createElement('li');
-      let a = document.createElement('a');
-      a.setAttribute('href', article.url);
-      a.setAttribute('target', '_blank');
-      a.textContent = article.title;
-      li.appendChild(a);
-      elNewsList.appendChild(li);
-    })
-  }).catch((error) => {
-    console.log(error)
-  })
-
+  getNewsJSON(urlForSearch, showNews);
 }
+
+if (elSearchForm) {
+  elSearchForm.addEventListener('submit', onSearchFormSubmit);
+}
+
+getNewsJSON(TOP_HEADLINES_URL, showNews);
